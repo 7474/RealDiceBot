5e6ed1624c43879856224f1c0642ac60e366b949
koudenpa <koudenpa@hotmail.com>
2020-09-10 02:18:24 +0900

    1d6ボットにした。

--- RealDiceBot/Bots/RealDiceBot.cs
+++ RealDiceBot/Bots/RealDiceBot.cs
@@ -3,6 +3,7 @@
 //
 // Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.10.2
 
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,17 +12,22 @@ using Microsoft.Bot.Schema;
 
 namespace RealDiceBot.Bots
 {
-    public class EchoBot : ActivityHandler
+    public class RealDiceBot : ActivityHandler
     {
+        private Random randomizer = new Random();
+
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var replyText = $"Echo: {turnContext.Activity.Text}";
+            var result = randomizer.Next(1, 7);
+            var replyText = $"1d6 = {result} !";
             await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hello and welcome!";
+            var welcomeText = "Hello. I'm Real DiceBot. " +
+                "I can roll 1d6 Only! " +
+                "Unfortunately it's not \"real\" now.";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)

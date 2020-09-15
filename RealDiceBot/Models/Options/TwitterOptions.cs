1f9c5619016e5d7a4dc1d86476889eeca4245e5a
koudenpa <koudenpa@hotmail.com>
2020-09-16 01:35:18 +0900

    DirectLineとTwitterの接続をホスティングする。

---
+++ RealDiceBot/Models/Options/TwitterOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealDiceBot.Models.Options
+{
+    public class TwitterOptions
+    {
+        public string ConsumerKey { get; set; }
+        public string ConsumerSecret { get; set; }
+        public string BearerToken { get; set; }
+        public string AccessToken { get; set; }
+        public string AccessTokenSecret { get; set; }
+    }
+}

b84d765091719815d7ba5bafc562617adebab3fb
koudenpa <koudenpa@hotmail.com>
2020-09-21 07:02:30 +0900

    モジュールのルーティング途上

--- RealDiceEdge/modules/RealDiceCognitiveModule/HttpRouter.cs
+++ RealDiceEdge/modules/RealDiceCognitiveModule/HttpRouter.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
-using System.Runtime.InteropServices.ComTypes;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace RealDiceCognitiveModule

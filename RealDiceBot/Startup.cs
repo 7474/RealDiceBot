fd9b45103765fac87f6d088520018eaf1d06e828
koudenpa <koudenpa@hotmail.com>
2020-10-02 02:49:59 +0900

    Update Startup.cs

--- RealDiceBot/Startup.cs
+++ RealDiceBot/Startup.cs
@@ -66,20 +66,20 @@ namespace RealDiceBot
 
             services.AddTwitterConversationAdapter(x => Configuration.Bind("Twitter", x));
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            // services.AddTransient<IBot, Bots.RealDiceBot>();
-            services.AddBot<Bots.RealDiceBot>(options =>
-            {
-                //var appId = Configuration.GetSection("MicrosoftAppId").Value;
-                //var appPassword = Configuration.GetSection("MicrosoftAppPassword").Value;
-                //options.CredentialProvider = new SimpleCredentialProvider(appId, appPassword);
+            services.AddTransient<IBot, Bots.RealDiceBot>();
+//             services.AddBot<Bots.RealDiceBot>(options =>
+//             {
+//                 //var appId = Configuration.GetSection("MicrosoftAppId").Value;
+//                 //var appPassword = Configuration.GetSection("MicrosoftAppPassword").Value;
+//                 //options.CredentialProvider = new SimpleCredentialProvider(appId, appPassword);
 
-                ILogger logger = LoggerFactory.CreateLogger<Bots.RealDiceBot>();
-                options.OnTurnError = async (context, exception) =>
-                {
-                    logger.LogError($"Exception caught : {exception}");
-                    await context.SendActivityAsync("Sorry, it looks like something went wrong.");
-                };
-            });
+//                 ILogger logger = LoggerFactory.CreateLogger<Bots.RealDiceBot>();
+//                 options.OnTurnError = async (context, exception) =>
+//                 {
+//                     logger.LogError($"Exception caught : {exception}");
+//                     await context.SendActivityAsync("Sorry, it looks like something went wrong.");
+//                 };
+//             });
 
             //var basePath = Env.ContentRootPath;
             var baseUrl = new Uri(Configuration["BaseUrl"]);

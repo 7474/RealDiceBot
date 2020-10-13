f7c49121b7ae94e6ce679b73a1e464247c059921
koudenpa <koudenpa@hotmail.com>
2020-10-14 02:30:40 +0900

    ビデオの配信処理を抽象化。

--- RealDiceEdge/modules/RealDiceCameraCvModule/VideoOutputStream.cs
+++ RealDiceEdge/modules/RealDiceCameraCvModule/VideoOutputStream.cs
@@ -6,43 +6,42 @@ using System.Timers;
 
 namespace RealDiceCameraCvModule
 {
-    public class VideoOutputStream : IDisposable
+    public class VideoOutputStream : AbstractVideoOutputStream
     {
         private VideoWriter videoWriter;
-        private Timer frameTimer;
-        private ConcurrentQueue<Mat> frameQueue;
-        private Mat lastFrame;
-        private bool isBusy;
         public string FileName { get; private set; }
 
         public VideoOutputStream(string fileName, FourCC fourcc, double fps, Size frameSize, bool isColor = true)
+            : base(fps)
         {
             FileName = fileName;
-            frameQueue = new ConcurrentQueue<Mat>();
             videoWriter = new VideoWriter(fileName, fourcc, fps, frameSize, isColor);
-            frameTimer = new Timer(1000.0 / fps);
-            frameTimer.Elapsed += WriteFrame;
         }
 
-        private void WriteFrame(object sender, ElapsedEventArgs e)
+        protected override void WriteFrame(object sender, ElapsedEventArgs e)
         {
             isBusy = true;
             try
             {
-                WriteLog("WriteFrame");
+                //WriteLog("WriteFrame");
                 Mat image = Read();
                 if (image != null)
                 {
-                    WriteLog("  image");
-                    if (lastFrame != null) { lastFrame.Dispose(); }
-                    lastFrame = image;
+                    //WriteLog("  image");
+                    //if (lastFrame != null) { lastFrame.Dispose(); }
+                    //lastFrame = image;
                     videoWriter.Write(image);
+                    image.Dispose();
                 }
-                else if (lastFrame != null)
+                //else if (lastFrame != null)
+                //{
+                //    WriteLog("  lastFrame");
+                //    // XXX どうもフレーム毎に位置情報が付加されている感じがする
+                //    videoWriter.Write(lastFrame);
+                //}
+                else
                 {
-                    WriteLog("  lastFrame");
-                    // XXX どうもフレーム毎に位置情報が付加されている感じがする
-                    videoWriter.Write(lastFrame);
+                    WriteLog("WriteFrame: Skip");
                 }
             }
             catch (Exception ex)
@@ -55,61 +54,21 @@ namespace RealDiceCameraCvModule
             }
         }
 
-        public void Dispose()
+        protected override void Dispose(bool disposing)
         {
             try
             {
+                base.Dispose(disposing);
                 if (videoWriter != null && !videoWriter.IsDisposed)
                 {
                     videoWriter.Release();
                     videoWriter.Dispose();
                 }
-                frameTimer.Dispose();
             }
             catch (Exception ex)
             {
                 WriteLog(ex.Message);
             }
-        }
-
-        public void Start()
-        {
-            frameTimer.Start();
-        }
-
-        public void Stop()
-        {
-            // https://docs.microsoft.com/en-us/dotnet/api/system.timers.timer.stop?view=netcore-2.1#examples
-            // タイマー処理が終わるまで待つサンプルが提示されているけれど
-            // 面倒くさいので簡易にやる
-            frameTimer.Stop();
-            while (isBusy) { Task.Delay(50).Wait(); }
-        }
-
-        public void Write(Mat image)
-        {
-            frameQueue.Enqueue(image.Clone());
-            while (frameQueue.Count > 2)
-            {
-                var disposeImage = Read();
-                if (disposeImage != null) { disposeImage.Dispose(); }
-            }
-        }
-
-        private Mat Read()
-        {
-            Mat image;
-            frameQueue.TryDequeue(out image);
-
-            // 成否に関らず返す。
-            return image;
-        }
-
-        static void WriteLog(string message)
-        {
-            Console.WriteLine(
-                "[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:fff") + "]"
-                + " " + message);
         }
     }
 }
